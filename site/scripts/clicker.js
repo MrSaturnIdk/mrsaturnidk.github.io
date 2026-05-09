51130e188cefe704b96880b1f66d3d8544caac3b
MrSaturnIdk <abrahm3314@gmail.com>
2026-05-09 11:24:38 -0500

    the button just plain explodes now

--- site/scripts/clicker.js
+++ site/scripts/clicker.js
@@ -1,11 +1,8 @@
-const btn_and_explode = document.querySelector(".button_and_explode");
 const beta = document.querySelector("#beta");
 const textelem = document.querySelector("#mad_text");
 const end = document.querySelector("#endscreen");
 const explode = document.querySelector("#explode")
 const btn = document.querySelector("#button")
-const speed = 10;
-let y_px = 0;
 explode.style.display = "none";
 textelem.style.display = "none";
 end.style.display = "none";
@@ -15,27 +12,12 @@ let anti_double = false;
 if ((localStorage.getItem("explosion")) !== null) {
    btn.style.display = "none"; 
    btn.disabled = true;
-   textelem.textContent = "Notice: Some idiot broke it, come back another time";
+   beta.textContent = "Notice: Some idiot broke it, come back another time";
 }
 function wait_explode() {
     return new Promise((resolve) => {
         explode.addEventListener("ended", resolve, {once: true});
     });
-}
-function fall() {
-    y_px += speed;
-    btn_and_explode.style.top = y_px + "px";
-    if (y_px < floor_px) {
-        requestAnimationFrame(fall);
-    } else {
-        btn_and_explode.style.top = floor_px + "px";
-        explode.style.display = "block";
-        async function explosion() {
-            explode.play();
-            await wait_explode();
-        }
-        text_shit();
-    }
 }
 function text_shit() {
     textelem.style.display = "block";
@@ -75,11 +57,16 @@ function cooltext(prompt,elem) {
 
     type();
 }
-function fall_start() {
+function explode() {
     if (anti_double) return;
     anti_double = true;
-    const rect = btn_and_explode.getBoundingClientRect();
-    y_px = rect.top;
-    floor_px = window.innerHeight - btn_and_explode.offsetHeight;
-    requestAnimationFrame(fall);
+    btn.style.display = "none";
+    explode.style.display = "block";
+        async function explosion() {
+            explode.play();
+            await wait_explode();
+        }
+        text_shit();
+    }
+}
 }
