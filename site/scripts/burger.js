aac43d7e266b17b709eb4b99611689fd0f7d0a2e
MrSaturnIdk <abrahm3314@gmail.com>
2026-05-10 13:07:17 -0500

    beta shi

--- site/scripts/burger.js
+++ site/scripts/burger.js
@@ -1,6 +1,7 @@
 const load = document.querySelector("#loading");
 const text = document.querySelector("#burger_text")
 const wait = (miliseconds) => new Promise(resolve => setTimeout(resolve, miliseconds));
+let displayable_time;
 // I'm too lazy for proper error handling lol
 let time = localStorage.getItem("stare_time");
 if (time == null) {
@@ -10,10 +11,24 @@ if (time == null) {
 else {
     console.log("Data refreshed")
 }
+function calc_time(seconds) {
+    let second = seconds % 60;
+    let minutes = Math.floor(seconds/60);
+    let hours = Math.floor(minutes/60);
+    let days = Math.floor(hours/24); // how the fuck would you achieve past this
+    let years = Math.floor(days/365);
+    let decades = Math.floor(years/10);
+    let centuries = Math.floor(decades/10);
+    let millenia = Math.floor(centuries/10);
+    let returnable;
+    return returnable;
+
+}
 // epic loop trust
 async function time_loop() {
     while (true) {
-        text.textContent = `You have stared at the hamburger for ${time} seconds`;
+        displayable_time = calc_time(time);
+        text.textContent = `You have stared at the hamburger for ${displayable_time} seconds`;
         await wait(1000);
         time++;
     }
