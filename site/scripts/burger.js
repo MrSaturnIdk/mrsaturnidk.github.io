fdb5a10c63746ea2e2a2cac6a1b4671add6c5305
MrSaturnIdk <abrahm3314@gmail.com>
2026-05-10 14:54:43 -0500

    time values yay

--- site/scripts/burger.js
+++ site/scripts/burger.js
@@ -11,24 +11,53 @@ if (time == null) {
 else {
     console.log("Data refreshed")
 }
-function calc_time(seconds) {
-    let second = seconds % 60;
-    let minutes = Math.floor(seconds/60);
-    let hours = Math.floor(minutes/60);
-    let days = Math.floor(hours/24); // how the fuck would you achieve past this
-    let years = Math.floor(days/365);
-    let decades = Math.floor(years/10);
-    let centuries = Math.floor(decades/10);
-    let millenia = Math.floor(centuries/10);
-    let returnable;
+function make_displayable_time(seconds) {
+    const units = [
+    {label: "second", modulo: 60, divide: 1},    
+    {label: "minutes", modulo: 60, divide: 60},
+    {label: "hour", modulo: 24, divide: 60},
+    {label: "day", modulo: 365, divide: 24}, //how the fuck do you get past days
+    {label: "year", modulo: 10, divide: 365},
+    {label: "decade", modulo: 1, divide: 10}
+    ];
+    let returnable = "";
+    let next = seconds;
+    for (let i = 0; i < units.length; i++) {
+        const {label, modulo, divide} = units[i];
+        const next_modued = next % modulo;
+        if (next_modued == 1) {
+            returnable = `${next_modued} ${label}, ` + returnable;
+        } 
+        else if (next_modued == 0) {/*empty*/} 
+        else {
+            returnable = `${next_modued} ${label}s, ` + returnable;
+        }
+        next = Math.floor(next/divide);
+    }
+    returnable = returnable.trim();
+    if (returnable.endsWith(",")) {
+        returnable = returnable.slice(0,-1);
+    }
+    const last_comma = returnable.lastIndexOf(",");
+    const first_comma = returnable.indexOf(",");
+    if (last_comma !== -1) {
+        if (last_comma == first_comma) {
+            returnable = `${returnable.slice(0, last_comma)} and${returnable.slice(last_comma +1)}`;
+        }
+        else {
+        returnable = `${returnable.slice(0, last_comma)}, and${returnable.slice(last_comma + 1)}`;
+        }
+    }
     return returnable;
-
 }
 // epic loop trust
 async function time_loop() {
     while (true) {
-        displayable_time = calc_time(time);
-        text.textContent = `You have stared at the hamburger for ${displayable_time} seconds`;
+        displayable_time = make_displayable_time(time);
+        if (displayable_time === "") {
+            displayable_time = "0 seconds";
+        }
+        text.textContent = `You have stared at the hamburger for ${displayable_time}.`;
         await wait(1000);
         time++;
     }
