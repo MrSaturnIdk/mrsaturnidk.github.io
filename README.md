8b5761d6d152483306306c7cb06d847f35441fc8
Mr Saturn <abrahm3314@gmail.com>
2026-05-18 17:17:57 -0500

    bugfixes, more commentary, etc. etc.

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ I mean what else could it possibly be
 # License
 Licensed under [AGPL-3.0-or-later](./LICENSE)
 
-(The web projects)
-
 # Contributing
 If you want to add something (like bugfixes for the projects, your own project, or something),
 read [this](./CONTRIBUTING.md).
--- developer-template/template.js
+++ developer-template/template.js
@@ -1,11 +1,13 @@
+// I needed a showcase for javascript, so all this does is tell you the amount of time you have spent on the page is even or odd
 const LOADING_SCREEN = document.querySelector('#loading-screen'); // notice SCREAMING_SNAKE_CASE usage for const
 const MAIN_TEXT = document.querySelector('#main-text');
 const WAIT = (miliseconds) => new Promise(resolve => setTimeout(resolve, miliseconds)); // wait script
 const IS_EVEN = (number) => number % 2 === 0;
-let time = 0;
+let time = 0; // note camelCase for normal vars
 let timeEvenDisplay = 'odd';
-class UselessAndUnprefferedClassWithFunctionForChangingTextContentPurelyForCasingExampleInWhichNameIsExtremelyLongForAbsolutelyNoReason { // notice pascal case for classes, if you could
-    constructor(textElement) {
+// I'm too lazy to add this, but localStorage items should have camelCase
+class UselessAndUnprefferedClassWithFunctionForChangingTextContentPurelyForCasingExampleInWhichNameIsExtremelyLongForAbsolutelyNoReason { // no, theres nothing at the end muhahah
+    constructor(textElement) { // don't ask why it's so long... just note the PascalCase
         this.textElementToChange = textElement;
     }
     changeText() {
@@ -13,7 +15,7 @@ class UselessAndUnprefferedClassWithFunctionForChangingTextContentPurelyForCasin
     }
 }
 const TEXT_CRAP_CLASS = new UselessAndUnprefferedClassWithFunctionForChangingTextContentPurelyForCasingExampleInWhichNameIsExtremelyLongForAbsolutelyNoReason(MAIN_TEXT);
-async function timeLoop() { // note camelcase for functions
+async function timeLoop() { // note camelCase for functions
     while (true) {
         if (IS_EVEN(time)) {
             timeEvenDisplay = 'even'; 
